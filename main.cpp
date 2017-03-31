d126318490a5a1c86c4d330ab0b0b157af19c439
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2017-03-31 16:47:04 +0200

    Fuses, 2kB for boot block size

--- main.cpp
+++ main.cpp
@@ -9,5 +9,15 @@
 #include <avr/fuse.h>
 #include <avr/boot.h>
 
+FUSES =
+{
+	.low = (FUSE_CKSEL0 & FUSE_CKSEL2 & FUSE_CKSEL3 & FUSE_SUT0 & FUSE_CKDIV8),
+	.high = (FUSE_BOOTSZ0 & FUSE_BOOTSZ1 & FUSE_SPIEN & FUSE_BOOTRST),
+	.extended = EFUSE_DEFAULT,
+};
 
+int main(void)
+{
+	
+}
 
