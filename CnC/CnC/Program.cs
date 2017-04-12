0e270a2cd488e4b165b9bd2962dc9702a8728a91
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2017-04-12 20:38:25 +0200

    Basic Command and Conquere project

---
+++ CnC/CnC/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SmartTable bootloader C&C software by Tomasz Jaworski");
+
+            if (SerialPort.GetPortNames().Length == 0)
+            {
+                Console.WriteLine("No serial ports available, quitting...");
+                return;
+            }
+
+            Console.Write("Available serial ports: {0}",
+                String.Join(", ", SerialPort.GetPortNames()));
+
+
+
+        }
+    }
+}
