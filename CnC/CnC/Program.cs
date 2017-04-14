4eaecfa23b2e960658b2f83e7e254596a2d48546
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2017-04-15 00:45:53 +0200

    Edited

--- CnC/CnC/Program.cs
+++ CnC/CnC/Program.cs
@@ -12,16 +12,16 @@ namespace CnC
 {
     struct Endpoint
     {
-        public MemoryStream ms;
+        public int address;
         public SerialPort sp;
 
-        public Endpoint(SerialPort sp)
+        public Endpoint(SerialPort sp, int addr)
         {
             this.sp = sp;
-            this.ms = new MemoryStream();
+            this.address = addr;
         }
-    }
 
+    }
     class Program
     {
         static void Main(string[] args)
@@ -31,12 +31,11 @@ namespace CnC
 
             MemoryMap mm = new MemoryMap(0x10000);
             IntelHEX16Storage st = new IntelHEX16Storage(mm);
-            st.Load(@"d:\praca\projekty\SmartTable\SmartTableDriver\SmartTableFirmware\Debug\SmartTableFirmware.hex");
+            st.Load(@"d:\praca\projekty\SmartTable\atmega328p-rs485-bootloader\Debug\atmega328p_bootloader.hex");
             mm.Dump("test.txt");
 
 
-            if (SerialPort.GetPortNames().Length == 0)
-            {
+            if (SerialPort.GetPortNames().Length == 0) {
                 Console.WriteLine("No serial ports available, quitting...");
                 return;
             }
@@ -44,53 +43,42 @@ namespace CnC
             Console.WriteLine("Available serial ports: {0}",
                 String.Join(", ", SerialPort.GetPortNames()));
 
+            List<SerialPort> ports = ListAndOpenSerialPorts();
 
-            // open all ports
-            //SerialPort[] ports = SerialPort.GetPortNames().Select(x => new SerialPort(x, 19200, Parity.Even, 8, StopBits.One)).ToArray();
-            List<Endpoint> endpoints = new List<Endpoint>();
-            foreach (String port_name in SerialPort.GetPortNames()) {
-                try {
-                    SerialPort sp = new SerialPort(port_name, 19200, Parity.Even, 8, StopBits.One);
-                    Console.Write("Openning {0}... ", sp.PortName);
-                    sp.ReadTimeout = 200;
-                    sp.Open();
-                    Console.WriteLine("Ok");
-
-                    endpoints.Add(new Endpoint(sp)); // use only opened ports
-                
-                } catch(Exception ex) {
-                    Console.WriteLine("Error: {0}", ex.Message);
-                }
-            }
-                
-
-
-            // send advertisement to all bootloader clients
-            byte[] req;
-            Console.WriteLine("*** TURN ON all bootloader clients and press any key to processed...");
-            Console.Write("Sending CnC Advertisemen to {0} serial ports: ", endpoints.Count);
-            while (!Console.KeyAvailable) {
-                req = new byte[] { (byte)'A' };
-                foreach (Endpoint ep in endpoints)
-                    ep.sp.Write(req, 0, 1);
-                Thread.Sleep(100);
-                Console.Write('.');
-
-            }
-            Console.WriteLine();
+            // send advertisement to all devices
+            SendAdvertisement(ports);
 
             // purge buffers
-            Thread.Sleep(100);
-            foreach (Endpoint ep in endpoints) {
-                ep.sp.DiscardInBuffer();
-                ep.sp.DiscardOutBuffer();
-            }
+            PurgeSerialPorts(ports);
 
-            // send challenge 
-            Console.WriteLine("Sending Challenge... ");
-            req = new byte[] { (byte)'C' };
-            foreach (Endpoint ep in endpoints)
-                ep.sp.Write(req, 0, 1);
+            // get list of devices for each serial port
+            List<Endpoint> endpoints = new List<Endpoint>();
+            foreach (SerialPort sp in ports)
+                AcquireDevicesOnSerialPort(endpoints, sp);
+
+            // show geathered devices
+            ShowDevices(endpoints.ToArray());
+
+
+
+        }
+
+        private static void ShowDevices(Endpoint[] endpoints)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nListing {0} acquired device(s): ", endpoints.Length);
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            foreach(Endpoint ep in endpoints) {
+                Console.WriteLine("   Device 0x{0:X2} on {1}", ep.address, ep.sp.PortName);
+            }
+        }
+
+        private static void AcquireDevicesOnSerialPort(List<Endpoint> endpoints, SerialPort sp)
+        {
+            Console.WriteLine("Sending Challenge to serial port {0}... ", sp.PortName);
+            byte[] req = new byte[] { (byte)'C' };
+            sp.Write(req, 0, 1);
             Console.WriteLine("Ok. ");
 
             // wait for them
@@ -101,31 +89,87 @@ namespace CnC
             byte[] buffer = new byte[32];
 
             DateTime start = DateTime.Now;
-            while((DateTime.Now - start).TotalMilliseconds <= 3000) {
-                foreach (Endpoint ep in endpoints) {
-                    try {
-                        int read = ep.sp.Read(buffer, 0, buffer.Length);
-                        ep.ms.Write(buffer, 0, read);
-                        Console.WriteLine("[{0} on {1}]", string.Join(",", buffer.Take(read).Select(x => "0x" + x.ToString("X2")).ToArray()), ep.sp.PortName);
-                    } catch(TimeoutException tex) {
-                        //
-                    }
+            MemoryStream ms = new MemoryStream();
+            while ((DateTime.Now - start).TotalMilliseconds <= 3000) {
+                try {
+                    int read = sp.Read(buffer, 0, buffer.Length);
+                    ms.Write(buffer, 0, read);
+                    Console.WriteLine("[{0} on {1}]", string.Join(",", buffer.Take(read).Select(x => "0x" + x.ToString("X2")).ToArray()), sp.PortName);
                 }
-
+                catch (TimeoutException tex) {
+                    //
+                }
                 Thread.Sleep(dt);
             }
 
-            // concatenate streams
-            MemoryStream ms = new MemoryStream();
-            foreach (Endpoint ep in endpoints) {
-                ep.ms.Seek(0, SeekOrigin.Begin);
-                ms.Write(ep.ms.GetBuffer(), 0, (int)ep.ms.Length);
-            }
 
-            // parse concatenated streams
+            // parse the acquired stream
+            ms.Seek(0, SeekOrigin.Begin);
+            BinaryReader br = new BinaryReader(ms);
+            while (br.PeekChar() != -1) {
+                int code = br.Read();
+                if (code != 'c') // challenge response
+                    continue;
 
+                int addr = br.Read(); // bootloader address
+                if (addr == -1)
+                    continue;
 
-
+                endpoints.Add(new Endpoint(sp, addr));
             }
         }
+
+        private static void PurgeSerialPorts(List<SerialPort> ports)
+        {
+            Thread.Sleep(100);
+            foreach (SerialPort sp in ports) {
+                sp.DiscardInBuffer();
+                sp.DiscardOutBuffer();
+            }
+        }
+
+        private static void SendAdvertisement(List<SerialPort> ports)
+        {
+            byte[] req;
+            Console.WriteLine("*** TURN ON all devices and press any key to processed...");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("\nSending CnC Advertisemen to {0} serial ports: ", ports.Count);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            while (!Console.KeyAvailable) {
+                req = new byte[] { (byte)'A' };
+                foreach (SerialPort sp in ports)
+                    sp.Write(req, 0, 1);
+                Thread.Sleep(100);
+                Console.Write('.');
+
+            }
+
+            Console.WriteLine(" Done.");
+        }
+
+        private static List<SerialPort> ListAndOpenSerialPorts()
+        {
+
+            // open all ports
+            List<SerialPort> ports = new List<SerialPort>();
+            foreach (String port_name in SerialPort.GetPortNames()) {
+                try {
+                    SerialPort sp = new SerialPort(port_name, 19200, Parity.Even, 8, StopBits.One);
+                    Console.Write("Openning {0}... ", sp.PortName);
+                    sp.ReadTimeout = 200;
+                    sp.Open();
+                    Console.WriteLine("Ok");
+
+                    ports.Add(sp); // use only opened ports
+
+                }
+                catch (Exception ex) {
+                    Console.WriteLine("Error: {0}", ex.Message);
+                }
+            }
+
+            return ports;
+        }
+    }
 }
