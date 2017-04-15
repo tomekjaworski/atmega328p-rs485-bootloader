e57ffb243aa7031108b3069674bcfd0b3f1ab732
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2017-04-15 13:43:04 +0200

    Added simple static Fifo buffer

---
+++ CnC/CnC/FifoBuffer.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CnC
+{
+
+	public class FifoBuffer
+	{
+		private byte[] buffer;
+		private int position;
+
+
+		public int Count { get { return this.position; } }
+
+		public FifoBuffer(int buffer_size)
+		{
+			this.buffer = new byte[buffer_size];
+			this.position = 0;
+		}
+
+
+		public void Write(byte[] bytes, int offset, int count)
+		{
+			Array.Copy(bytes, offset, this.buffer, this.position, count);
+			this.position += count;
+		}
+
+
+		public byte[] PeekBytes(int offset, int count)
+		{
+			//TODO: zamiast tych korekcji lepiej rzucacaæ wyj¹tkiem
+			offset = Math.Max(offset, 0);
+			count = Math.Max(count, 0);
+
+			offset = Math.Min(offset, this.position); // ograniczenie pozycji startowej
+			count = Math.Min(position - offset, count); // ograniczenie ilosci danych
+
+			byte[] b = new byte[count];
+			Array.Copy(this.buffer, offset, b, 0, count);
+			return b;
+		}
+
+		public void DeleteFirstBytes(int count)
+		{
+			int cnt = this.position - count;
+			Array.Copy(this.buffer, count, this.buffer, 0, cnt);
+			Array.Clear(this.buffer, cnt, count);
+			position -= count;
+		}
+
+		public Int32 PeekUInt32(int offset)
+		{
+			return System.Net.IPAddress.NetworkToHostOrder(BitConverter.ToInt32(this.buffer, offset));
+		}
+	}
+}
