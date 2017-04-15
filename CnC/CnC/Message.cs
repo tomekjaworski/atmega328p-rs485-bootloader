dad337635e9df8a63c7d3f79b41dcda320e89f92
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2017-04-15 13:43:20 +0200

    Added message builder

---
+++ CnC/CnC/Message.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnC
+{
+    public class Message
+    {
+        byte address;
+        MessageType type;
+        byte[] payload;
+
+        public byte Address => this.address;
+        public MessageType Type => this.type;
+        public byte[] Payload => this.payload;
+
+        public byte[] Binary => ToBinary();
+        public int BinarySize => 1 + 1 + 1 + this.payload.Length + 2;
+
+        public Message(byte address, MessageType type)
+            : this(address, type, new byte[0])
+        { }
+
+        public Message(byte address, MessageType type, byte[] payload)
+        {
+            if (payload == null)
+                throw new NullReferenceException("payload");
+            if (payload.Length > 200)
+                throw new ArgumentOutOfRangeException("payload");
+
+            this.address = address;
+            this.type = type;
+            this.payload = payload.Clone() as byte[];
+
+        }
+
+        private byte[] ToBinary()
+        {
+            byte[] msg = new byte[this.BinarySize];
+            msg[0] = address;
+            msg[1] = (byte)type;
+            msg[2] = (byte)payload.Length;
+
+            Array.Copy(payload, 0, msg, 3, payload.Length);
+
+            UInt16 checksum = 0;
+            for (int i = 0; i < payload.Length + 3; i++)
+                checksum += msg[i];
+
+            msg[3 + payload.Length + 0] = (byte)((checksum & 0xFF00) >> 8);
+            msg[3 + payload.Length + 1] = (byte)(checksum & 0x00FF);
+
+            return msg;
+        }
+
+
+    }
+}
---
+++ CnC/CnC/MessageType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnC
+{
+    public enum MessageType : byte
+    {
+        BL_COMMAND_ACTIVATE = (byte)'A',
+        BL_COMMAND_DEACTIVATE = (byte)'B',
+        BL_COMMAND_PING = (byte)'?',
+        BL_COMMAND_REBOOT = (byte)'R',
+
+        BL_COMMAND_READ_PAGE = (byte)'X',
+        BL_COMMAND_WRITE_PAGE = (byte)'W'
+    }
+}
