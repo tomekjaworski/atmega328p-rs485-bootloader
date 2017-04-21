db209b98951dd0c3cee50fd88a4a3c41855eed99
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2017-04-21 23:56:47 +0200

    Moved bootloader code in Release in .text section to its last 512 words

--- fuses.cpp
+++ fuses.cpp
@@ -5,16 +5,24 @@
  *  Author: Tomasz Jaworski
  */ 
 
- #include <avr/io.h>
- #include <avr/fuse.h> // after IO
- #include <avr/lock.h>
+#include <avr/io.h>
+#include <avr/fuse.h> // after IO
+#include <avr/lock.h>
 
- FUSES =
- {
-	 .low = 0xFF,
-	 .high = (FUSE_SPIEN & FUSE_EESAVE & FUSE_BOOTSZ0 & FUSE_BOOTSZ1 & FUSE_BOOTRST),
-	 .extended = FUSE_BODLEVEL0 & FUSE_BODLEVEL1,
- };
+FUSES =
+{
+	.low = 0xFF,
+#if defined (DEBUG)
+	// DEBUG
+	// 2048 words for bootloader
+	.high = (FUSE_SPIEN & FUSE_EESAVE & FUSE_BOOTRST & FUSE_BOOTSZ0 & FUSE_BOOTSZ1 ),
+#else
+	// RELEASE
+	// 512 words for bootloader
+	.high = (FUSE_SPIEN & FUSE_EESAVE & FUSE_BOOTRST & FUSE_BOOTSZ0 /*& FUSE_BOOTSZ1*/ ),
+#endif
+	.extended = FUSE_BODLEVEL0 & FUSE_BODLEVEL1,
+};
 
- LOCKBITS = (BLB1_MODE_2);
+LOCKBITS = (BLB1_MODE_2);
 
