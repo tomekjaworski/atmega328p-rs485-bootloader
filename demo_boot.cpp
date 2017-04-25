b3349b8ee52127d7fbc2e370a493d01d581ae651
Tomasz Jaworski <tjaworski@iis.p.lodz.pl>
2017-04-25 18:45:03 +0200

    Added blinking LED for Arduino Pro Mini in default user program

--- demo_boot.cpp
+++ demo_boot.cpp
@@ -10,8 +10,13 @@
 #include <stddef.h>
 #include "uart.h"
 
-void ___boot_demo(void) __attribute__ ((__used__, section (".BL")));
+//
+// For Arduino Pro Mini 328 the LED_TOGGLE toggles on-board LED diode.
+// If this bootloader is used on other boards, change this line or remove it completely
+#define LED_TOGGLE do { PORTB ^= _BV(PORTB5); } while (0);
 
+
+void ___boot_demo(void) __attribute__ ((__used__, section (".BL")));
 void ___boot_demo(void)
 {
 	uint8_t arr[3];
@@ -23,9 +28,15 @@ void ___boot_demo(void)
 
 	while(1)
 	for (uint8_t i = 0; i < 3; i++) {
+
+		// send a byte
 		UCSR0A |= _BV(TXC0);
 		UDR0 = arr[i];
 		while (!(UCSR0A & _BV(TXC0)));
+
+		// toggle some led
+		LED_TOGGLE;
+
 #if defined (DEBUG)
 		_delay_ms(100);
 #else
